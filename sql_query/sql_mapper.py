199ab2fa7505cfb41f7bbbafc7d130e00af0ee12
梁宵 <liangxiao@sunlands.com>
2019-01-21 20:44:31 +0800

    get every day follow data sql

---
+++ sql_query/sql_mapper.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/1/21 11:17
+# @Author  : liangxiao
+# @Site    : 
+# @File    : sql_mapper.py
+# @Software: PyCharm
+
+GET_ALIGNMENT = """
+SELECT
+     DISTINCT drag_opportunity_distribution_log.`id`,
+     drag_opportunity_distribution_log.`opp_id`,
+     drag_opportunity_distribution_log.`account`,
+     drag_opportunity_distribution_log.`sub_distribution_type`,
+     drag_opportunity_distribution_log.`operator_time`,
+     drag_opportunity_distribution_log.`quantum_id`,
+     if(drag_opportunity_follow_log.`consultant_account` is not null, 1, 0) as followed
+ FROM
+     drag_opportunity
+ LEFT JOIN
+     drag_quantum_config
+ ON
+     drag_opportunity.`quantum_id` = drag_quantum_config.`quantum_id`
+ LEFT JOIN
+     drag_opportunity_distribution_log
+ ON
+     drag_opportunity_distribution_log.`opp_id` = drag_opportunity.`id`
+ LEFT OUTER JOIN
+     drag_opportunity_follow_log
+ ON
+     drag_opportunity_follow_log.`opportunity_id` = drag_opportunity.`id`
+ and 
+     drag_opportunity_follow_log.`consultant_account` = drag_opportunity_distribution_log.`account`
+ WHERE
+     drag_opportunity_distribution_log.`distribution_type` = 'AUTO_DISTRIBUTION'
+ AND
+ (
+     drag_opportunity_distribution_log.`sub_distribution_type`= 'SUB_AUTO_ONLINE_DISTRIBUTION'
+ OR
+     drag_opportunity_distribution_log.`sub_distribution_type`= 'SUB_AUTO_PREDICT_DISTRIBUTION'
+ OR
+     drag_opportunity_distribution_log.`sub_distribution_type`= 'SUB_AUTO_PREDICT_RECYCLE_DISTRIBUTION'
+ OR 
+     drag_opportunity_distribution_log.`sub_distribution_type`= 'SUB_AUTO_PREDICT_ONLINE_DISTRIBUTION'
+ OR 
+     drag_opportunity_distribution_log.`sub_distribution_type`= 'SUB_AUTO_PREDICT_MESSAGE_DISTRIBUTION'
+ OR
+     drag_opportunity_distribution_log.`sub_distribution_type`= 'SUB_AUTO_MESSAGE_DISTRIBUTION'
+ OR
+     drag_opportunity_distribution_log.`sub_distribution_type`= 'SUB_AUTO_RECYCLE_DISTRIBUTION'
+ OR
+     drag_opportunity_distribution_log.`sub_distribution_type`= 'SUB_AUTO_INDIVIDUATION_DISTRIBUTION'
+ )
+ AND
+     drag_quantum_config.`config_value` IN ('MOBILE', 'WECHAT', 'MOBILE_WECHAT')
+ AND
+     drag_opportunity_distribution_log.`operator_time` >= "20190116"
+ AND
+     drag_opportunity_distribution_log.`operator_time` < "20190117"
+"""
