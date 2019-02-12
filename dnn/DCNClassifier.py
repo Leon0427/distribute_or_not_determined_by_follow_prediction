856477d126e39b5892d3ea922e9dc8483e0907a4
梁宵 <liangxiao@sunlands.com>
2019-02-12 17:45:57 +0800

    [major commit] 1.add dcn classifier constructed by keras

--- dnn/DCNClassifier.py
+++ dnn/DCNClassifier.py
@@ -16,7 +16,8 @@ from keras.optimizers import Adam
 from keras import backend as K
 from sklearn.metrics import roc_auc_score
 import numpy as np
-from keras.layers import Flatten, Lambda,  Add
+from keras.layers import Flatten, Lambda,  Add, RepeatVector
+from keras.layers.merge import concatenate
 from keras import layers
 
 class DCNClassifier:
@@ -29,6 +30,7 @@ class DCNClassifier:
     def _model(self):
         # --- start with deep neural net work
         input = Input(batch_shape=[self.batch_size,self.input_size])
+        print type(input)
         x1 = Dense(units=64,input_shape=[self.batch_size,self.input_size])(input)
         x1_batch = BatchNormalization()(x1)
         x1_act = Activation("relu")(x1_batch)
@@ -38,34 +40,27 @@ class DCNClassifier:
         x2_batch = BatchNormalization()(x2)
         x2_act = Activation("relu")(x2_batch)
         x2_drop = Dropout(0.5)(x2_act)
+        print type(x2_drop)
 
         # ---- cross network
-        cross_input = K.reshape(input, (self.batch_size,1,self.input_size))
-        print cross_input,"====="
+        def reshp1(tensor):
+            # using K.reshape to reshape causing a error named
+            # // AttributeError: 'NoneType' object has no attribute 'inbound_nodes' //
+            # resolve this problem by Lambda : keras abstract its own layers to
+            # manipulate tensor, not compatible with tf.functions,
+            # using tf under layer: Lambda for convenience if function is stateless
+            shape = (self.batch_size,1,self.input_size)
+            return K.reshape(tensor,shape)
+        cross_input = Lambda(reshp1)(input)
         cross = CrossLayer(output_dim=cross_input.shape[2], num_layer=3, name="cross_layer")(cross_input)
-        cross = K.reshape(cross,(self.batch_size, self.input_size))
-        print x2_drop.shape,">>>>"
-        print cross.shape,"<<<"
+        def reshp2(tensor):
+            shape = (self.batch_size, self.input_size)
+            return K.reshape(tensor,shape)
+        cross = Lambda(reshp2)(cross)
 
-        concatenated = K.concatenate([x2_drop, cross])
-
+        concatenated = concatenate([x2_drop, cross])
         out = Dense(units=1, activation="sigmoid")(concatenated)
         model = Model(inputs=[input], outputs=out)
-
-
-
-        # model = Sequential()
-        # model.add(Dense(units=64, input_dim=self.input_size))
-        # model.add(BatchNormalization())
-        # model.add(Activation("relu"))
-        # model.add(Dropout(0.5))
-        #
-        # model.add(Dense(units=32))
-        # model.add(BatchNormalization())
-        # model.add(Activation("relu"))
-        # model.add(Dropout(0.5))
-        #
-        # model.add(Dense(units=1, activation="sigmoid"))
 
         model.compile(loss="mse", optimizer=Adam(lr=0.001))
 
@@ -88,7 +83,6 @@ class CrossLayer(layers.Layer):
         super(CrossLayer, self).__init__(**kwargs)
 
     def build(self, input_shape):
-        print input_shape
         self.input_dim = input_shape[2]
         self.W = []
         self.bias = []
@@ -100,16 +94,27 @@ class CrossLayer(layers.Layer):
     def call(self, input):
         for i in range(self.num_layer):
             if i == 0:
-                constant = np.eye(input.shape[2])
-                constant = np.repeat(constant[np.newaxis, ...],input.shape[0],axis=0)
-                print "constant shape: %s" % str(constant.shape)
-                print "bias shape %s" % self.bias[0].shape
-                print self.bias[i] * Input(tensor=K.constant(constant))
-                print (self.W[i] * K.batch_dot(K.reshape(input, (-1, self.input_dim, 1)), input)).shape
-                cross = Lambda(lambda x: Add()([K.sum(self.W[i] * K.batch_dot(K.reshape(x, (-1, self.input_dim, 1)), x), 1, keepdims = True),  K.reshape(K.repeat(self.bias[i],input.shape[0]),input.shape), x]))(input)
+                # constant = np.eye(input.shape[2])
+                # constant = np.repeat(constant[np.newaxis, ...],input.shape[0],axis=0)
+                # print "constant shape: %s" % str(constant.shape)
+                # print "bias shape %s" % self.bias[0].shape
+                # print self.bias[i] * Input(tensor=K.constant(constant))
+                # print (self.W[i] * K.batch_dot(K.reshape(input, (-1, self.input_dim, 1)), input)).shape
+                func = lambda x: Add()([
+                    K.sum(self.W[i] * K.batch_dot(K.reshape(x, (-1, self.input_dim, 1)), x,), 1, keepdims = True), # W x X x XT (1024*307*307)
+                    K.reshape(RepeatVector(input.shape[0])(self.bias[i]),input.shape), # (1024, 1, 307)
+                    x # x (1024, 1 , 307)
+                ])
+                cross = Lambda(func)(input)
             else:
-                cross = Lambda(lambda x: Add()([K.sum(self.W[i] * K.batch_dot(K.reshape(x, (-1, self.input_dim, 1)), input), 1, keepdims = True), K.reshape(K.repeat(self.bias[i],input.shape[0]),input.shape), input]))(cross)
+                func = lambda x: Add()([
+                    K.sum(self.W[i] * K.batch_dot(K.reshape(x, (-1, self.input_dim, 1)), input), 1, keepdims = True),
+                    K.reshape(RepeatVector(input.shape[0])(self.bias[i]),input.shape),
+                    input
+                ])
+
+                cross = Lambda(func)(cross)
         return Flatten()(cross)
 
     def compute_output_shape(self, input_shape):
-        return (None, self.output_dim)
+        return (input_shape[0], None, self.output_dim)
